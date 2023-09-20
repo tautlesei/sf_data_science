cb8669b4966d7b7511ecb75c070320b354869f49
tautlesei <petra.maliukevich@gmail.com>
2023-09-20 20:44:50 +0300

    initial commit

---
+++ test.py
@@ -0,0 +1,9 @@
+
+
+from game_v2 import random_predict, score_game
+
+print ("in test.py files variable __name__ is: " + __name__)
+
+result = score_game(random_predict)
+random_predict(result - 1)
+
